cb13f4ffde54c29eb8693c0992d4dc500ee6b318
farhan-saf7 <farhanazadshah7@gmail.com>
2026-02-12 21:41:48 +0530

    day-14 part-2

--- Backend/day-14/src/controllers/auth.controller.js
+++ Backend/day-14/src/controllers/auth.controller.js
@@ -1,5 +1,6 @@
 const userModel = require("../models/user.model")
 const crypto = require("crypto")
+const bcrypt = require("bcryptjs")
 const jwt = require("jsonwebtoken")
 
 async function registerController (req, res) {
@@ -31,7 +32,7 @@ async function registerController (req, res) {
         })
     }
 
-    const hash = crypto.createHash('sha256').update(password).digest('hex')
+    const hash = await bcrypt.hash(password, 10)
 
     const user = await userModel.create({
         username,
@@ -78,9 +79,8 @@ async function loginController (req, res) {
             message: "user not found"
         })
     }
-    const hash = crypto.createHash("sha256").update(password).digest("hex")
 
-    const isPasswordValid = hash === user.password
+    const isPasswordValid = await bcrypt.compare(password, user.password)
 
     if(!isPasswordValid){
         return res.status(401).json({
