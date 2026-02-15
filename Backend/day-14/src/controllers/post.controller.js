4b45490e84c961fc90decd1e6b327bffd11efdae
farhan-saf7 <farhanazadshah7@gmail.com>
2026-02-15 14:31:57 +0530

    day-14 part-4

--- Backend/day-14/src/controllers/post.controller.js
+++ Backend/day-14/src/controllers/post.controller.js
@@ -47,4 +47,72 @@ async function createPostController(req,res){
 
 }
 
-module.exports = {createPostController}
+async function getPostController(req,res){
+    const token = req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message: "unauthorized user"
+        })
+    }
+    let decoded;
+    try{
+        decoded = jwt.verify(token,process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message: "uniauthorized user"
+        })
+    }
+
+    const userId = decoded.id
+
+    const posts = await postModel.find({
+        user: userId
+    })
+
+    res.status(200).json({
+        message: "posts fetched sucessfully",
+        posts
+    })
+}
+
+async function getPostDetailsController(req,res){
+   const token = req.cookies.token
+    if(!token){
+        return res.status(401).json({
+            message: "unauthorized user"
+        })
+    }
+    let decoded;
+    try{
+        decoded = jwt.verify(token,process.env.JWT_SECRET)
+    }catch(err){
+        return res.status(401).json({
+            message: "uniauthorized user"
+        })
+    } 
+    const userId = decoded.id
+    const postId = req.params.postId
+
+    const post = await postModel.findById(postId)
+
+    if(!post){
+        return res.status(404).json({
+            message: "post not found"
+        })
+    }
+
+    const isValidUser = post.user.toString() === userId
+
+    if(!isValidUser){
+        return res.status(403).json({
+            message: "Forbidden Content"
+        })
+    }
+
+    return res.status(200).json({
+        message: "post fetched sucessfully",
+        post
+    })
+}
+
+module.exports = {createPostController,getPostController,getPostDetailsController}
