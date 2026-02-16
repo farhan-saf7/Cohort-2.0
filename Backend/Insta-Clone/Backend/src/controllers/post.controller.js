76459f7c8e28c380cbb79222109dd80800145251
farhan-saf7 <farhanazadshah7@gmail.com>
2026-02-16 23:55:51 +0530

    day-14 part-5

--- Backend/Insta-Clone/Backend/src/controllers/post.controller.js
+++ Backend/Insta-Clone/Backend/src/controllers/post.controller.js
@@ -1,31 +1,12 @@
 const postModel = require("../models/post.model")
 const ImageKit = require("@imagekit/nodejs")
 const {toFile} = require("@imagekit/nodejs")
-const jwt = require("jsonwebtoken")
 
 const imagekit = new ImageKit({
     privateKey: process.env.IMAGEKIT_PRIVATE_KEY
 })
 
 async function createPostController(req,res){
-    console.log(req.body,req.file)
-
-    const token = req.cookies.token
-
-    if(!token){
-        return res.status(401).json({
-            message: "Token not provided, Unauthorized access"
-        })
-    }
-
-    let decoded = null
-    try{
-        decoded = jwt.verify(token,process.env.JWT_SECRET)
-    } catch(err){
-        return res.status(401).json({
-            message: "user unauthorized"
-        })
-    }
 
     const file = await imagekit.files.upload({
         file: await toFile(Buffer.from(req.file.buffer),"file"),
@@ -36,7 +17,7 @@ async function createPostController(req,res){
     const post = await postModel.create({
         caption: req.body.caption,
         imgUrl: file.url,
-        user: decoded.id
+        user: req.user.id
     })
 
     res.status(201).json({
@@ -48,22 +29,8 @@ async function createPostController(req,res){
 }
 
 async function getPostController(req,res){
-    const token = req.cookies.token
-    if(!token){
-        return res.status(401).json({
-            message: "unauthorized user"
-        })
-    }
-    let decoded;
-    try{
-        decoded = jwt.verify(token,process.env.JWT_SECRET)
-    }catch(err){
-        return res.status(401).json({
-            message: "uniauthorized user"
-        })
-    }
-
-    const userId = decoded.id
+    
+    const userId = req.user.id
 
     const posts = await postModel.find({
         user: userId
@@ -76,21 +43,8 @@ async function getPostController(req,res){
 }
 
 async function getPostDetailsController(req,res){
-   const token = req.cookies.token
-    if(!token){
-        return res.status(401).json({
-            message: "unauthorized user"
-        })
-    }
-    let decoded;
-    try{
-        decoded = jwt.verify(token,process.env.JWT_SECRET)
-    }catch(err){
-        return res.status(401).json({
-            message: "uniauthorized user"
-        })
-    } 
-    const userId = decoded.id
+   
+    const userId = req.user.id
     const postId = req.params.postId
 
     const post = await postModel.findById(postId)
