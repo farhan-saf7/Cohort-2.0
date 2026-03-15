d55103e9ec545fa17c06a509756da92cde4e3b68
farhan-saf7 <farhanazadshah7@gmail.com>
2026-03-15 20:49:53 +0530

    perpelxity-2.2

--- Backend/day-34/index.js
+++ Backend/day-34/index.js
@@ -1,7 +1,24 @@
 import "dotenv/config"
 import readline from "readline/promises"
 import { ChatMistralAI } from "@langchain/mistralai";
-import { HumanMessage } from "langchain";
+import { HumanMessage, tool, createAgent } from "langchain";
+import { sendEmail } from "./mail.services.js";
+import * as z from "zod"
+
+
+const emailTool = tool(
+    sendEmail,
+    {
+        name: "emailTool",
+        description: "use this tool to sends an email",
+        schema: z.object({
+            to: z.string().describe("The recipient's email address"),
+            html: z.string().describe("The HTML content of the email"),
+            subject: z.string().describe("The subject of the email")
+        })
+    }
+)
+
 
 const rl = readline.createInterface({
     input: process.stdin,
@@ -12,14 +29,19 @@ const model = new ChatMistralAI({
     model: "mistral-small-latest"
 });
 
-const message = []
+const agent = createAgent({
+    model,
+    tools: [ emailTool ]
+})
+
+const messages = []
 
 while (true) {
     const userInput = await rl.question("You: ")
-    message.push(new HumanMessage(userInput))
-    const response = await model.invoke(message)
-    message.push(response)
-    console.log("AI: " + response.content)
+    messages.push(new HumanMessage(userInput))
+    const response = await agent.invoke({
+        messages
+    })
+    messages.push(response.messages[ response.messages.length - 1 ])
+    console.log(`AI: ${response.messages[ response.messages.length - 1 ].content}`)
 }
-
-rl.close()
