1fd2917719bb388fb4b1d58e5b00b9bbadbc3968
farhan-saf7 <farhanazadshah7@gmail.com>
2026-03-17 17:55:45 +0530

    perpelxity-3

--- Backend/Perplexity/Backend/server.js
+++ Backend/Perplexity/Backend/server.js
@@ -1,8 +1,14 @@
 import "dotenv/config";
 import app from "./src/app.js";
 import connectDB from "./src/config/database.js";
+import http from "http"
+import { initSocket } from "./src/sockets/server.socket.js";
 
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 3000;
+
+const httpServer = http.createServer(app)
+
+initSocket(httpServer)
 
 connectDB()
     .catch((err) => {
@@ -10,6 +16,6 @@ connectDB()
         process.exit(1);
     });
 
-app.listen(PORT, () => {
+httpServer.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`);
 });
--- Backend/Perplexity/Backend/src/app.js
+++ Backend/Perplexity/Backend/src/app.js
@@ -1,10 +1,18 @@
 import express from "express";
 import cookieParser from "cookie-parser";
 import authRouter from "./routes/auth.routes.js";
+import morgan from "morgan"
+import cors from "cors"
 
 const app = express();
 
 // Middleware
+app.use(cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+    methods: [ "GET", "POST", "PUT", "DELETE" ],
+}))
+app.use(morgan("dev"));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
